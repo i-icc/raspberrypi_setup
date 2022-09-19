29c38af1ff9a5d6ef82fa0e0a9d96106115a83af
IchitoNitta <52018660+i-icc@users.noreply.github.com>
2022-09-19 15:55:19 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,4 @@ sudo apt update
 sudo apt upgrade
 sudo apt install -y git openssl libssl-dev libbz2-dev libreadline-dev libsqlite3-dev vim
 git clone https://github.com/yyuu/pyenv.git ~/.pyenv
-echo export PYENV_ROOT="$HOME/.pyenv" >> ~/.bash_profile
-echo export PATH="$PYENV_ROOT/bin:$PATH" >> ~/.bash_profile
-echo eval "$(pyenv init -)" >> ~/.bash_profile
+cp ./.bash_profile ~/.bash_profile
